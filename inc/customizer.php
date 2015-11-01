f8cddf6e516c366f9a229c87066c9308c273ac2e
Hardeep Asrani <me@hardeepasrani.com>
2015-11-02 04:06:56 +0530

    Added donation link.
    Added donation link the the customizer so people could support the
    development.

--- inc/customizer.php
+++ inc/customizer.php
@@ -3,15 +3,14 @@
  * Register colors and layout for the Theme Customizer.
 */
 
-function docpress_sanitize_checkbox( $input ) {
-    if ( $input == 1 ) {
-		return 1;
-	} else {
-		return '';
-	}
-}
 
 function docpress_customize_register($wp_customize) {
+
+	class DocPress_Support extends WP_Customize_Control {
+		public function render_content() {
+			echo __('If you like this theme and if it helped you with your business then please consider supporting the development <a target="_blank" href="http://www.hardeepasrani.com/donate/">by donating some money</a>. This theme is 100% free and will always be. <a target="_blank" href="http://www.hardeepasrani.com/donate/">Any amount, even $1.00, is appreciated :)</a>','docpress');
+		}
+	}
 
 	$wp_customize->add_panel( 'docpress_header', array(
 		'priority'	   => 40,
@@ -23,7 +22,12 @@ function docpress_customize_register($wp_customize) {
 	$wp_customize->get_section( 'header_image' )->panel = 'docpress_header';
 	$wp_customize->get_section( 'header_image' )->title = 'Background';
 	$wp_customize->get_section( 'header_image' )->priority = 5;
-	
+
+    $wp_customize->add_section('donate_section', array(
+        'priority' => 5,
+        'title' => __('Do You Like This Theme?', 'docpress')
+    ));
+
 	$wp_customize->add_section('docpress_header_text', array(
 		'priority' => 10,
 		'title' => __('Text', 'docpress'),
@@ -35,6 +39,14 @@ function docpress_customize_register($wp_customize) {
 		'title' => __('Search', 'docpress'),
 		'panel'  => 'docpress_header',
 	));
+
+	$wp_customize->add_setting( 'donate_section_main', array(
+		'sanitize_callback' => 'docpress_sanitize_text'
+	));
+
+	$wp_customize->add_control( new DocPress_Support( $wp_customize, 'donate_section_main', array(
+		'section' => 'donate_section',
+	)));
 
 	$wp_customize->add_setting('docpress_header_title', array(
 		'default' => __('Need Help? Try me!', 'docpress'),
@@ -98,6 +110,18 @@ function docpress_customize_register($wp_customize) {
 		'priority' => 15,
 		'settings' => 'docpress_header_search_button'
 	));
+
+	function docpress_sanitize_checkbox( $input ) {
+		if ( $input == 1 ) {
+			return 1;
+		} else {
+			return '';
+		}
+	}
+
+	function docpress_sanitize_text( $input ) {
+		return $input;
+	}
 }
 add_action('customize_register', 'docpress_customize_register');
 
