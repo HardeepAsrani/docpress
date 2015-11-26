607f951c01c33f33bbe6dd06232e1dd6e9c6c1ae
Hardeep Asrani <me@hardeepasrani.com>
2015-11-27 03:57:04 +0530

    Few improvements.
    Few stuff to make it even better, baby!

--- content-page.php
+++ content-page.php
@@ -1,4 +1,4 @@
-<article class="item">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
         <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
     </header>
--- content.php
+++ content.php
@@ -1,6 +1,5 @@
-
-                    <article id="post-<?php the_ID(); ?>" <?php post_class('doc-item'); ?>>
-                        <a class="title" title="<?php the_title(); ?>" href="<?php the_permalink(); ?>"> 
-							<h2><?php the_title(); ?> &rarr;</h2>
-						</a>
-					</article>
+<article id="post-<?php the_ID(); ?>" <?php post_class('doc-item'); ?>>
+	<a class="title" title="<?php the_title(); ?>" href="<?php the_permalink(); ?>"> 
+		<h2><?php the_title(); ?> &rarr;</h2>
+	</a>
+</article>
